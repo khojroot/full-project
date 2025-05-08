f5dc740a8d771a33a75a998fdf851e90792a5312
khojroot <92933992+khojroot@users.noreply.github.com>
2025-05-08 13:49:00 +0530

    ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+
+if command -v ansible &> /dev/null
+then
+    echo "✅ Ansible is already installed. Skipping installation."
+else
+    echo "🔧 Ansible not found. Installing EPEL and Ansible..."
+
 dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
 yum install ansible* -y
+
+    echo "✅ Ansible installation completed."
+fi
+
